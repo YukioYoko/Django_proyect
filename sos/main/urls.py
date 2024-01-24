fa8b04f963594a7933aa7bbd098bd9405dcf9a2e
YukioYoko <yukio.yokogawa141002@gmail.com>
2024-01-24 16:54:32 -0600

    added_repairs

--- sos/main/urls.py
+++ sos/main/urls.py
@@ -3,4 +3,5 @@ from  . import views
 
 urlpatterns = [
     path('', views.index, name='main'),
+    path('repairs', views.repairs, name='repairs'),
 ]
--- sos/main/views.py
+++ sos/main/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, "home.html", {})
+
+def repairs(request):
+    return render(request, "repairs.html", {})
